e9191e13cd90580183bc09af62e95fff049c7abc
Ing140943 <skladeeing@gmail.com>
2021-09-12 16:46:03 +0700

    Add 2 methods and end_date attribute

--- polls/models.py
+++ polls/models.py
@@ -7,16 +7,29 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+    end_date = models.DateTimeField('end dated', default=None, null=True)
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        """Check that polls is published recently."""
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        """Check that this poll is published"""
+        now = timezone.now()
+        return now >= self.pub_date
+
+    def can_vote(self):
+        """We can vote if we are in the duration of available polls."""
+        now = timezone.now()
+        return self.end_date >= now and self.is_published()
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
