d270814a9278fc92804626f961ca6cc569fdb649
Ing140943 <skladeeing@gmail.com>
2021-09-14 21:50:22 +0700

    Add message when we create the choice successfully

--- polls/views.py
+++ polls/views.py
@@ -57,4 +57,5 @@ def vote(request, question_id):
             # Always return an HttpResponseRedirect after successfully dealing
             # with POST data. This prevents data from being posted twice if a
             # user hits the Back button.
+            messages.success(request, "Your choice successfully recorded. Thank you.")
             return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
